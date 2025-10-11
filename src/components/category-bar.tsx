e640f84b02e5d055a517aa330b1fad9c110999d8
조아 <parkjoah@naver.com>
2025-10-11 21:21:43 +0900

    feat : add category state (#2)

--- src/components/category-bar.tsx
+++ src/components/category-bar.tsx
@@ -1,3 +1,6 @@
+"use client";
+import { useState } from "react";
+
 const categories = [
   "전체",
   "식품",
@@ -9,12 +12,17 @@ const categories = [
 ];
 
 export default function CategoryBar() {
+  const [category, setCategory] = useState("전체");
+
   return (
     <div className=" px-4 typo-r14 flex py-4 overflow-x-auto gap-2 whitespace-nowrap scroll-smooth [scrollbar-width:none] border-b border-[#F2F2F2]">
       {categories.map((cat, idx) => (
         <div
-          className="py-2 px-3 border-[1px] border-[#F2F2F2] rounded-[100px] "
+          className={`py-2 px-3 border-[1px] border-[#F2F2F2] rounded-[100px] cursor-pointer ${
+            cat === category ? "bg-[#000] text-white" : "bg-white"
+          }`}
           key={idx}
+          onClick={() => setCategory(cat)}
         >
           {cat}
         </div>
