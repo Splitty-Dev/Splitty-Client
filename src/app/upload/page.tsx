8bf6a37024a941439f704b5fbc1cbfe8365de2c1
조아 <parkjoah@naver.com>
2025-10-16 18:57:37 +0900

    feat : image uploads & delete

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -38,16 +38,58 @@ export default function UploadPage() {
   const oneProductPrice =
     totalPriceNum && totalNumNum ? (totalPriceNum / totalNumNum).toFixed(0) : 0;
 
+  const [images, setImages] = useState<File[]>([]);
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      const newFiles = Array.from(e.target.files);
+      setImages((prev) => [...prev, ...newFiles]);
+    }
+  };
+  const handleRemoveImage = (index: number) => {
+    setImages((prev) => prev.filter((_, i) => i !== index));
+  };
+
   return (
     <div className="relative pt-[47px] pb-[80px]">
       <UploadBottomSection price={`${oneProductPrice}원`} />
       <div className="px-4 py-3 fixed top-0 h-[95px] w-full bg-[white] pt-[47px] items-center flex border-b border-[#F2F2F2] ">
         <CloseButton />
       </div>
-      <section className="pl-4 py-3 px-4 ">
-        <div className="w-15 h-15 border-[1px] rounded-[4px] border-[#F2F2F2] flex justify-center items-center">
-          <Image src={camIcon} alt="사진" width={24} height={24} />
-        </div>
+      <section className="pl-4 py-3 px-4 flex gap-2">
+        <label
+          htmlFor="upload"
+          className="w-[60px] h-[60px] border border-[#F2F2F2] rounded-[4px] flex justify-center items-center cursor-pointer"
+        >
+          <Image src={camIcon} alt="사진 추가" width={24} height={24} />
+        </label>
+        <input
+          id="upload"
+          type="file"
+          accept="image/*"
+          multiple
+          className="hidden"
+          onChange={handleImageChange}
+        />
+
+        {/* 미리보기 */}
+        {images.map((file, index) => (
+          <div key={index} className="relative w-[60px] h-[60px]">
+            <Image
+              src={URL.createObjectURL(file)}
+              alt={`uploaded-${index}`}
+              width={60}
+              height={60}
+              className="object-cover rounded-[4px] w-full h-full"
+            />
+            <button
+              type="button"
+              onClick={() => handleRemoveImage(index)}
+              className="absolute top-0 right-0 bg-black bg-opacity-50 text-white rounded-full w-4 h-4 text-[10px]"
+            >
+              ×
+            </button>
+          </div>
+        ))}
       </section>
       <CategoryBar isAll={false} />
       <form className="px-4">
