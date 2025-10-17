111c39e50e699462d6bbd19d44ae22c80178b861
조아 <parkjoah@naver.com>
2025-10-17 13:08:00 +0900

    feat : implement search-result-page ui (#12)

--- src/app/search/_component_/search-bar.tsx
+++ src/app/search/_component_/search-bar.tsx
@@ -1,14 +1,44 @@
+"use client";
 import BackBkBtn from "@/components/back-bk-btn";
+import { useRouter, useSearchParams } from "next/navigation";
+
+import { useEffect, useState } from "react";
 
 export default function SearchBar() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const [search, setSearch] = useState("");
+
+  const q = searchParams.get("q");
+  useEffect(() => {
+    setSearch(q || "");
+  }, [q]);
+
+  const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+  const onSubmit = () => {
+    if (!search || q === search) return;
+    router.push(`/search?q=${search}`);
+  };
+  const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      onSubmit();
+    }
+  };
   return (
     <div className="flex px-4 py-3 justify-between gap-[10px]">
       <BackBkBtn />
       <input
         placeholder={`미아동 근처에서 검색`}
         className="bg-[#F2F2F2] typo-r14 flex-1 py-[5.5px] px-[14px] rounded-[4px] focus:outline-none"
+        onKeyDown={onKeyDown}
+        onChange={onChangeSearch}
+        value={search}
       />
-      <button className="typo-r12 text-[#8C8C8C]">검색</button>
+      <button className="typo-r12 text-[#8C8C8C]" onClick={onSubmit}>
+        검색
+      </button>
     </div>
   );
 }
--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,25 +1,45 @@
+"use client";
 import clock from "@/assets/icons/searchClock.svg";
 import close from "@/assets/icons/searchClose.svg";
+import ProductItem from "@/components/product-item";
+import { mock } from "@/mocks/mock";
 import Image from "next/image";
+import { useSearchParams } from "next/navigation";
 
 const recentList = ["에코 생수 500ml", "에코 생수 100ml"];
 
 export default function SearchPage() {
+  const searchParams = useSearchParams();
+  const query = searchParams.get("q");
   return (
     <div>
-      <div className="flex justify-between px-4 py-3">
-        <h2 className="typo-b14">최근검색</h2>
-        <button className="typo-r10 text-[#8C8C8C]">전체 삭제</button>
-      </div>
-      <div className="flex gap-2 flex-col">
-        {recentList.map((recent, idx) => (
-          <div key={idx} className="flex px-4 gap-2 ">
-            <Image src={clock} alt="최근" width={16} height={16} />
-            <p className="flex-1 typo-r14 text-[#757575]">에코 생수 500ml</p>
-            <Image src={close} alt="삭제" width={15} height={15} />
+      {!query && (
+        <div>
+          {query}
+          <div className="flex justify-between px-4 py-3">
+            <h2 className="typo-b14">최근검색</h2>
+            <button className="typo-r10 text-[#8C8C8C]">전체 삭제</button>
           </div>
-        ))}
-      </div>
+          <div className="flex gap-2 flex-col">
+            {recentList.map((recent, idx) => (
+              <div key={idx} className="flex px-4 gap-2 ">
+                <Image src={clock} alt="최근" width={16} height={16} />
+                <p className="flex-1 typo-r14 text-[#757575]">
+                  에코 생수 500ml
+                </p>
+                <Image src={close} alt="삭제" width={15} height={15} />
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+      {query && (
+        <div>
+          {mock.map((item) => (
+            <ProductItem product={item} key={item.id} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
