6d9ebc18a37d8bfc10ff3892cc969774990c432b
조아 <parkjoah@naver.com>
2025-11-02 02:52:25 +0900

    feat:  검색어 단일/전체삭제 연결(#30)

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -9,6 +9,7 @@ import { getMySearchRec } from "../api/search";
 import { useSearchProducts } from "@/hooks/useSearchProducts";
 import { useEffect, useRef } from "react";
 import { productType } from "@/types/product";
+import { apiFetch } from "../api";
 
 export default function SearchPage() {
   const queryKey = ["searchedRec"];
@@ -38,6 +39,17 @@ export default function SearchPage() {
     return () => observer.disconnect();
   }, [hasNextPage, fetchNextPage]);
 
+  const handleDeleteOneSearchHistory = async (searchHistoryId: number) => {
+    await apiFetch(`/member/me/search/${searchHistoryId}`, {
+      method: "DELETE",
+    });
+  };
+  const handleDeleteAllSearchHistory = async () => {
+    await apiFetch(`/member/me/search`, {
+      method: "DELETE",
+    });
+  };
+
   return (
     <div>
       {!keyword && (
@@ -45,7 +57,12 @@ export default function SearchPage() {
           {keyword}
           <div className="flex justify-between px-4 py-3">
             <h2 className="typo-b16 font-bold text-[16px]">최근검색</h2>
-            <button className="typo-r12 text-[#8C8C8C]">전체 삭제</button>
+            <button
+              className="typo-r12 text-[#8C8C8C]"
+              onClick={() => handleDeleteAllSearchHistory()}
+            >
+              전체 삭제
+            </button>
           </div>
           <div className="flex gap-2 flex-col typo-r16 text-[#757575]">
             {recentList.length > 0 ? (
@@ -53,7 +70,13 @@ export default function SearchPage() {
                 <div key={r.id} className="flex px-4 gap-2 ">
                   <Image src={clock} alt="최근" width={16} height={16} />
                   <p className="flex-1 ">{r.keyword}</p>
-                  <Image src={close} alt="삭제" width={15} height={15} />
+                  <Image
+                    src={close}
+                    alt="삭제"
+                    width={15}
+                    height={15}
+                    onClick={() => handleDeleteOneSearchHistory(r.id)}
+                  />
                 </div>
               ))
             ) : (
@@ -62,23 +85,13 @@ export default function SearchPage() {
           </div>
         </div>
       )}
+
       {keyword && (
         <div>
-          {keyword && (
-            <div>
-              {data?.pages.map((page, pageIdx) =>
-                page?.map((product: productType) => (
-                  <ProductItem
-                    key={`${pageIdx}-${product.id}`}
-                    product={product}
-                  />
-                ))
-              )}
-              <div
-                ref={observerRef}
-                className="h-12 flex justify-center items-center"
-              ></div>
-            </div>
+          {data?.pages.map((page, pageIdx) =>
+            page?.map((product: productType) => (
+              <ProductItem key={`${pageIdx}-${product.id}`} product={product} />
+            ))
           )}
           <div
             ref={observerRef}
